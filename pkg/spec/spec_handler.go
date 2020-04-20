ba1f74d06c5d27e35064ea78c004ba32687a5159
Kevin McDermott <bigkevmcd@gmail.com>
2020-04-20 15:30:07 +0100

    Refactor the event handling in the Spec handler.

--- pkg/spec/spec_handler.go
+++ pkg/spec/spec_handler.go
@@ -60,56 +60,17 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 // TODO: refactor to remove the duplication.
 func (h *Handler) pullRequest(ctx context.Context, evt *scm.PullRequestHook, w http.ResponseWriter) {
 	repo := fmt.Sprintf("%s/%s", evt.Repo.Namespace, evt.Repo.Name)
-	h.log.Infow("processing request", "repo", repo)
-	content, err := h.scmClient.FileContents(ctx, repo, pullRequestFilename, evt.PullRequest.Ref)
-	if git.IsNotFound(err) {
-		h.log.Infof("no pipeline definition found in %s", repo)
-		return
-	}
-	if err != nil {
-		h.log.Errorf("error fetching pipeline file: %s", err)
-		// TODO: should this return a 404?
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	parsed, err := Parse(bytes.NewReader(content))
-	if err != nil {
-		h.log.Errorf("error parsing pipeline definition: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	pr, err := Execute(parsed, evt, defaultPipelineRunPrefix)
-	if err != nil {
-		h.log.Errorf("error executing pipeline definition: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	created, err := h.pipelineClient.TektonV1beta1().PipelineRuns(h.namespace).Create(pr)
-	if err != nil {
-		h.log.Errorf("error creating pipelinerun file: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	b, err := json.Marshal(created)
-	if err != nil {
-		h.log.Errorf("error marshaling response: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	_, err = w.Write(b)
-	if err != nil {
-		h.log.Errorf("error writing response: %s", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	h.log.Infow("completed request")
+	h.handleEvent(ctx, repo, evt.PullRequest.Ref, pullRequestFilename, evt, w)
 }
 
 func (h *Handler) push(ctx context.Context, evt *scm.PushHook, w http.ResponseWriter) {
 	repo := fmt.Sprintf("%s/%s", evt.Repo.Namespace, evt.Repo.Name)
-	h.log.Infow("processing request", "repo", repo)
-	content, err := h.scmClient.FileContents(ctx, repo, pushFilename, evt.Ref)
+	h.handleEvent(ctx, repo, evt.Ref, pushFilename, evt, w)
+}
+
+func (h *Handler) handleEvent(ctx context.Context, repo, ref, filename string, evt scm.Webhook, w http.ResponseWriter) {
+	h.log.Infow(fmt.Sprintf("processing event '%T'", evt), "repo", repo)
+	content, err := h.scmClient.FileContents(ctx, repo, filename, ref)
 	if git.IsNotFound(err) {
 		h.log.Infof("no pipeline definition found in %s", repo)
 		return
